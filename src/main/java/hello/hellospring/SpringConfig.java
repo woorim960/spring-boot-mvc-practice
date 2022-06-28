8ec98128db93304caba47563be6d74a67db0f9b8
박우림 <woorimprog@gmail.com>
2022-06-28 17:22:32 +0900

    Create: sql ddl 생성

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
-package hello.hellospring;
+package hello.hellospring.controller;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import javax.sql.DataSource;
+import javax.swing.Spring;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+  private DataSource dataSource;
+
+  @Autowired
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
 
   @Bean
   public MemberService memberService() {
@@ -16,6 +27,7 @@ public class SpringConfig {
 
   @Bean
   public MemberRepository memberRepository() {
-    return new MemoryMemberRepository();
+//    return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 }
