8d5873e8f663d0eb11b8dce4e2f769d1f3788dc6
박우림 <woorimprog@gmail.com>
2022-06-30 14:32:20 +0900

    Add(jdbcTemplate): save, findById 기능 등록

--- src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -1,11 +1,18 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 public class JdbcTemplateMemberRepository implements MemberRepository {
 
@@ -18,12 +25,21 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
 
   @Override
   public Member save(Member member) {
-    return null;
+    SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+    jdbcInsert.withTableName("member").usingGeneratedKeyColumns("id");
+
+    Map<String, Object> parameters = new HashMap<>();
+    parameters.put("name", member.getName());
+
+    Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+    member.setId(key.longValue());
+    return member;
   }
 
   @Override
   public Optional<Member> findById(Long id) {
-    return jdbcTemplate.query("select * from member where id = ?");
+    List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper());
+    return result.stream().findAny();
   }
 
   @Override
@@ -34,5 +50,15 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
   @Override
   public List<Member> findAll() {
     return null;
+  }
+
+  private RowMapper<Member> memberRowMapper() {
+      return (rs, rowNum) -> {
+        Member member = new Member();
+        member.setId(rs.getLong("id"));
+        member.setName(rs.getString("name"));
+        return member;
+      };
+    }
   }
 }
